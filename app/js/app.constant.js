23f695ce3c41df93b4d32b2f45a33c00727e9b3f
coach88 <alexsemenyuk88@gmail.com>
2014-12-18 12:41:06 +0200

    Fix client and server urls.

--- app/js/app.constant.js
+++ app/js/app.constant.js
@@ -1,3 +1,3 @@
-app.constant('SERVER_URL', 'http://192.168.28.2:8080');
-app.constant('CLIENT_URL', 'http://192.168.28.2:8000');
+app.constant('SERVER_URL', 'http://dev.itdep.bsuir.by:8080');
+app.constant('CLIENT_URL', 'http://dev.itdep.bsuir.by:8000');
 
