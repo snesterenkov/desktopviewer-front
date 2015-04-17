b3b200cf6322eddb412ac8d141a18b29de3c9ed0
Vladislav Pisetskiy <playonly231@gmail.com>
2015-04-17 11:48:25 +0300

    fixed urls

--- app/js/app.constant.js
+++ app/js/app.constant.js
@@ -1,3 +1,3 @@
-app.constant('SERVER_URL', 'http://itdep.bsuir.by:8080');
-app.constant('CLIENT_URL', 'http://itdep.bsuir.by:8000');
+app.constant('SERVER_URL', 'http://dev.itdep.bsuir.by:8080');
+app.constant('CLIENT_URL', 'http://dev.itdep.bsuir.by:8000');
 
