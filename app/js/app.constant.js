f740e05c8f02585becccf6221b3bedb4713d6c3d
coach88 <alexsemenyuk88@gmail.com>
2015-08-07 12:52:00 +0300

    Fixed url for server and client

--- app/js/app.constant.js
+++ app/js/app.constant.js
@@ -1,3 +1,3 @@
-app.constant('SERVER_URL', 'http://dev.itdep.bsuir.by:8080');
-app.constant('CLIENT_URL', 'http://dev.itdep.bsuir.by:8000');
+app.constant('SERVER_URL', 'http://192.168.28.99:8080');
+app.constant('CLIENT_URL', 'http://192.168.28.99:8000');
 
