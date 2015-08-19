de676c78d905fd0efe3a541c81ec53d96e1624b1
V.Pisetskiy <rockman52@ya.ru>
2015-08-19 11:33:29 +0300

    Add methods for add and remove users from project

--- app/js/controller/projectController.js
+++ app/js/controller/projectController.js
@@ -1,6 +1,7 @@
 'use strict';
 
-app.controller('ProjectController', ['$injector', '$scope', 'Project', 'freeUsers', 'changeStatusProject', 'openDepartment', function($injector, $scope, Project, freeUsers, changeStatusProject, openDepartment) {
+app.controller('ProjectController', ['$injector', '$scope', 'Project', 'freeUsers',
+    'detailUpdateProject', 'changeStatusProject', 'openDepartment', function($injector, $scope, Project, freeUsers, detailUpdateProject, changeStatusProject, openDepartment) {
 
     $injector.invoke(BaseController, this, {
         $scope: $scope,
@@ -23,25 +24,30 @@ app.controller('ProjectController', ['$injector', '$scope', 'Project', 'freeUser
         openDepartment.openDepartment().success(successOpenDepartment);
     };
 
-    $scope.editProjectMembers = function(project){
-        $scope.selectedProject = project;
-        var successFindFreeUsers = function (users) {
-            $scope.freeUsers = users;
-        };
-        freeUsers.getFreeUsers(project.id).success(successFindFreeUsers);
-    }
-
-    $scope.clearAddList = function(){
-        $scope.usersForAdd = {};
-    }
-
     $scope.getProjectItemForUpdate = function(item){
         $scope.getItemForUpdate(item);
         var successOpenDepartment = function (departments) {
             $scope.openDepartmentItems = departments;
         };
         openDepartment.openDepartment().success(successOpenDepartment);
-    }
+    };
+
+    $scope.getFreeUsers = function(projectId){
+        var successFindFreeUsers = function (users) {
+            $scope.freeUsers = users;
+        };
+        freeUsers.getFreeUsers(projectId).success(successFindFreeUsers);
+        };
+
+    $scope.editProjectMembers = function(project){
+        $scope.clearAddList();
+        $scope.currentItem = project;
+        $scope.getFreeUsers($scope.currentItem.id);
+    };
+
+    $scope.clearAddList = function(){
+        $scope.usersForAdd = {};
+    };
 
     $scope.addUserToAddList = function(user){
         if($scope.usersForAdd[user.id]){
@@ -49,13 +55,37 @@ app.controller('ProjectController', ['$injector', '$scope', 'Project', 'freeUser
         } else{
             $scope.usersForAdd[user.id] = user;
         }
-    }
+    };
+
+    $scope.successDetailUpdate = function(updatedProject){
+        $scope.currentItem.name = updatedProject.name;
+        $scope.currentItem.departmentId = updatedProject.departmentId;
+        $scope.currentItem.departmentDTO = updatedProject.departmentDTO;
+        $scope.currentItem.userDTOs = updatedProject.userDTOs;
+        $scope.getFreeUsers($scope.currentItem.id);
+    };
 
     $scope.addUsersToProject = function(){
-        $scope.clearAddList();
-    }
+        var updateItem = angular.copy($scope.currentItem);
+        for(var key in $scope.usersForAdd){
+            updateItem.userDTOs.push(angular.copy($scope.usersForAdd[key]));
+        }
+        detailUpdateProject.detailUpdate(updateItem.id, updateItem).success($scope.successDetailUpdate);
+    };
 
-    $scope.selectedProject = {};
+    $scope.removeUserFromProject = function(userId){
+        var updateItem = angular.copy($scope.currentItem);
+        updateItem.userDTOs = [];
+        $scope.currentItem.userDTOs.forEach(function(value, index){
+            if(value.id != userId){
+                updateItem.userDTOs.push(value);
+            }
+        });
+        console.log(updateItem);
+        detailUpdateProject.detailUpdate(updateItem.id, updateItem).success($scope.successDetailUpdate);
+    };
+
+    $scope.currentItem = {};
     $scope.usersForAdd = {};
     $scope.usersForDelete = {};
 }]);
