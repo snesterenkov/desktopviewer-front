9fed46d28d7e698c18d5f95f1250084682c00c76
coach88 <alexsemenyuk88@gmail.com>
2015-08-25 12:19:07 +0300

    Added support russian locale for calendar

---
+++ app/js/directive/calendarDirective.js
@@ -0,0 +1,26 @@
+'use strict';
+
+directives.directive('picker', function () {
+
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attributes, ctrl) {
+
+            var picker = element.data("DateTimePicker");
+
+            element.on('dp.change', function (event) {
+                scope.$apply(function () {
+                    var date = picker.date();
+                    scope.goToSelectedDate(date);
+                });
+            });
+            element.on('dp.change', function (event) {
+                scope.$apply(function () {
+                    var date = picker.date();
+                    scope.goToSelectedDate(date);
+                });
+            });
+        }
+    };
+});
