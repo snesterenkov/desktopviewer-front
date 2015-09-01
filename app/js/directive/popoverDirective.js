231eb56e4eb5e2e638a7e184cd52cf70976c6be7
coach88 <alexsemenyuk88@gmail.com>
2015-09-01 14:19:27 +0300

    Changed size popover on dashboard page

--- app/js/directive/popoverDirective.js
+++ app/js/directive/popoverDirective.js
@@ -12,11 +12,11 @@ directives.directive('customPopover', function ($compile, snapshot) {
             $(el).popover({
                 trigger: 'hover',
                 html: true,
-                content:  $compile('<label class="control-label label-success">{{message}}</label>' +
+                content: $compile('<label class="control-label label-success">{{message}}</label>' +
                     '<img style="width: 450px; height: 300px; padding: 1px;" ng-src="data:image/jpg;base64,{{snapshot}}" class="img-responsive">'+
                     '<label class="control-label" style="float: left">{{time}}&nbsp</label>'+
                     '<div class="progress progress-striped">'+
-                    '<div class="progress-bar progress-bar-success" style="width:{{percent}}%"></div>'+
+                        '<div class="progress-bar progress-bar-success" style="width:{{percent}}%"></div>'+
                     '</div>')(scope),
                 placement: attrs.popoverPlacement
             });
