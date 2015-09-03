10a12494ed0e52aef057144a78c842fcca5160f0
coach88 <alexsemenyuk88@gmail.com>
2015-09-03 12:33:40 +0300

    Changed template for popover

--- app/js/directive/popoverDirective.js
+++ app/js/directive/popoverDirective.js
@@ -12,11 +12,16 @@ directives.directive('customPopover', function ($compile, snapshot) {
             $(el).popover({
                 trigger: 'hover',
                 html: true,
-                content: $compile('<label class="control-label label-success">{{message}}</label>' +
+                content: $compile(
                     '<img style="width: 450px; height: 300px; padding: 1px;" ng-src="data:image/jpg;base64,{{snapshot}}" class="img-responsive">'+
-                    '<label class="control-label" style="float: left">{{time}}&nbsp</label>'+
+                    '<label>Snapshot taken: &nbsp {{time}}</label><br/>' +
+                    '<label>Memo: &nbsp {{message}}</label><br/>'+
+                    '<label>Active window: &nbsp {{note}}</label><br/>'+
+                    '<label style="float: left">Activity: &nbsp </label>'+
+                    '<div>'+
                     '<div class="progress progress-striped">'+
                         '<div class="progress-bar progress-bar-success" style="width:{{percent}}%"></div>'+
+                    '</div>'+
                     '</div>')(scope),
                 placement: attrs.popoverPlacement
             });
