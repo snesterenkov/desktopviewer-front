9cdc309d681de0eb815d8c9a4cc1eab1d9994e73
coach88 <alexsemenyuk88@gmail.com>
2015-09-10 12:18:27 +0300

    Fixed message's style for snapshot

--- app/js/directive/popoverDirective.js
+++ app/js/directive/popoverDirective.js
@@ -16,8 +16,8 @@ directives.directive('customPopover', function ($compile, snapshot) {
                     '<img style="border-radius: 0.5em; width: 450px; height: 300px; padding-top: 20px;padding-bottom: 20px;padding-left: 15px; padding-right: 15px; border: 2px solid green;"' +
                         ' ng-src="data:image/jpg;base64,{{snapshot}}" class="img-responsive"><br/>'+
                     '<label class="labels">Snapshot taken:</label> <label>{{time}}</label><br/>' +
-                    '<label class="labels">Memo: </label> <label> {{message}}</label><br/>'+
-                    '<label class="labels">Active window: </label> <div class="box"><div class="text"><label> {{note}}</label></div></div>'+
+                    '<label class="labels">Memo: </label> <label style="width: 200px;"> {{message}}</label><br/>'+
+                    '<label class="labels">Active window: </label> <div class="box"><div><label> {{note}}</label></div></div>'+
                     '<label class="labels">Activity: &nbsp </label>'+
                     '<div style="max-width: 450px">'+
                     '<div class="progress progress-striped">'+
